f48dcfae7ef60d52aba0184725f49f6cfd7a83c8
Mikko Vuopala <mikko.vuopala@aalto.fi>
2021-05-22 15:51:55 +0300

    index update ja procfilen lisäys

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ if (persons.some(p => p.name === body.name)) {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 }) 
