77ff8d1628c5a80ad07e43d5059445d5fcf499d3
Mikko Vuopala <mikko.vuopala@aalto.fi>
2021-05-22 19:56:31 +0300

    3.11 tehty fullstacki herokuun

---
+++ build/precache-manifest.eb61b2da67e7427cd1292f62536aba5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efee2dfe4b7bff93ee46a17406b2cc5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20f6903feff5ce383155",
+    "url": "/static/css/main.e835366d.chunk.css"
+  },
+  {
+    "revision": "16ee3d411164fc5738fc",
+    "url": "/static/js/2.e657b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e657b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f6903feff5ce383155",
+    "url": "/static/js/main.6ac7ee17.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
